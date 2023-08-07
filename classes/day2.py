6a36616a93769abec53463e2f9b22d8c40c07248
Pasunurib <Prakashbanu362@gmail.com>
2023-08-07 21:55:28 +0530

    final  commit

--- classes/day2.py
+++ classes/day2.py
@@ -1,7 +1,10 @@
-A = int(input("firstnumber="))
-B = int(input("secondnumber="))
+#A = int(input("firstnumber="))
+#B = int(input("secondnumber="))
 # (Arthematic Operators)
 # addition operator
+
+A=10
+B=20
 C = A + B
 print(C)
 
@@ -24,3 +27,74 @@ print(C)
 # True (or) False operator
 C = A > B
 print(C)
+
+#(Assignment operators)
+#addition
+A=10
+A+=20
+print(A)
+
+#substract
+A=10
+A-=20
+print(A)
+
+#multiply
+A=10
+A*=20
+print(A)
+
+#division
+A=10
+A/=20
+print(A)
+
+#percentage
+A=10
+A%=20
+print(A)
+
+#(Comparasion operators)
+#less than
+A=10
+B=20
+C=A<B
+print(C)
+
+#Great than
+
+A=10
+B=20
+C=A>B
+print(C)
+
+#lessthan equal to
+A=10
+B=20
+C=A<=B
+print(C)
+
+#greater than equal to
+A=10
+B=20
+C=A>=B
+print(C)
+
+#equal to
+A=10
+B=20
+C=A==B
+print(C)
+
+#not equal to
+A=10
+B=20
+C=A!=B
+print(C)
+
+
+
+
+
+
+
