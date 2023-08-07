84af86a4116849485ff795399ed27fc4c279c267
Pasunurib <Prakashbanu362@gmail.com>
2023-08-07 21:33:48 +0530

    Initial commit

---
+++ classes/day2.py
@@ -0,0 +1,26 @@
+A = int(input("firstnumber="))
+B = int(input("secondnumber="))
+# (Arthematic Operators)
+# addition operator
+C = A + B
+print(C)
+
+# substract operator
+C = A - B
+print(C)
+
+# muliply opertor
+C = A * B
+print(C)
+
+# division operator
+C = A / B
+print(C)
+
+# percentage operator
+C = A % B
+print(C)
+
+# True (or) False operator
+C = A > B
+print(C)
