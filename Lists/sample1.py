9e3dc983794fd84df7ad89be89b4871c62225376
Pasunurib <Prakashbanu362@gmail.com>
2023-08-08 18:59:14 +0530

    final  commit

--- Lists/sample1.py
+++ Lists/sample1.py
@@ -14,14 +14,21 @@ Pos3=Nums[2]
 Pos4=Nums[3]
 Pos5=Nums[4]
 Pos6=Nums[5]
+
 print(Pos1,Pos2,Pos3,Pos4,Pos5,Pos6)
+
+
 '''print continous words 
 b using this line of code 
 we can easily print the cord '''
+
 Name="Bhanu\n"
 print((Name + " ") * 10)
+
+
 '''Integers are converted into string 
 by using type function'''
+
 Numbers=str(Nums)
 print(Numbers)
 type1=type(Numbers)
