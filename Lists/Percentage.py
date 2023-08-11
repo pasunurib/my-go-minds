dd56db95fcf84fafe7f55670a25f642c5c073150
Pasunurib <Prakashbanu362@gmail.com>
2023-08-11 18:42:57 +0530

    final  commit

--- Lists/Percentage.py
+++ Lists/Percentage.py
@@ -1,8 +1,11 @@
-S4=50
-S5=60
-S6=71
-total=S4+S5+S6
-print(total)
+def sub_jects(points):
+    sum_of_points=sum(points)
+    total_subjects=len(points)
+    total_points=int(60)
+    average_points = sum_of_points / total_points * 100
+    roundednumber = round(average_points, 2)
+    print("Your Average-points are=",roundednumber)
 
-Average=S4/int(input("total marks by student s4:"))*100
-print(Average)
+points=[9,10,7,10,10,9]
+sub_jects(points)
+
