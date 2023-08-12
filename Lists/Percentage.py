847e825d4707c0d31190819bc71da932df0389eb
Pasunurib <Prakashbanu362@gmail.com>
2023-08-12 12:16:34 +0530

    final  commit

--- Lists/Percentage.py
+++ Lists/Percentage.py
@@ -10,3 +10,27 @@ points=[9,10,7,10,10,9]
 sub_jects(points)
 
 
+points=int(input("Enter the points ="))
+
+'''Grades of Subjects '''
+def Grades_010():
+    if points >= 10:
+        print("Grade A1  ")
+    elif points >= 9:
+        print("Grade A2  ")
+    elif points >= 8:
+        print("Grade B1  ")
+    elif points >= 7:
+        print("Grade B2  ")
+    elif points >= 6:
+        print("Grade C1 ")
+    elif points >= 5:
+        print("Grade C2 ")
+    else:
+        print("Grade F   ")
+
+Grades_010()
+
+
+
+
