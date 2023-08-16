0ebc81e5a1f84ac5a4b8e6e9244054323c45d920
Pasunurib <Prakashbanu362@gmail.com>
2023-08-16 17:15:13 +0530

    final  commit

--- Lists/task.py
+++ Lists/task.py
@@ -1,6 +1,6 @@
 '''To find uppercase or lower case '''
 
-str1=input("Enter a Value = ")
+str1=str(input("Enter a Value = "))
 
 if str1.islower():
     print(str1,"is a Lower Case")
@@ -8,10 +8,10 @@ elif str1.isupper():
     print(str1,"is a Upper Case")
 elif str1.isdigit():
     print(str1,"is a Digit")
-elif str1.isalnum():
-    print(str1,"is a Alphanumeric")
 elif str1.isalpha():
     print(str1,"is a Alphabet")
+elif str1.isalnum():
+    print(str1,"is a Alphanumeric")
 else :
     print(str1, "is a Speical Character")
 
