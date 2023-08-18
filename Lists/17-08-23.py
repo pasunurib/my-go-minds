f75958c7bb3db6696fe519d3fd62dc760a3f81a2
Pasunurib <Prakashbanu362@gmail.com>
2023-08-18 15:09:26 +0530

    "final commit"

--- Lists/17-08-23.py
+++ Lists/17-08-23.py
@@ -1,7 +1,7 @@
-Subjects=["Telugu","Hindi","English","Maths","Science","Social"]
+Subject = ["Telugu ", "Hindi  ", "English", "Maths  ", "Science", "Social "]
 Marks=[85,95,65,95,95,85]
 Grade=["A2","A1","B2","A1","A1","A2"]
 Points=[9,10,6,10,10,9]
 
 for i in range(6):
-    print(Subjects[i],' = ', Marks[i],' = ',Grade[i],' = ',Points[i],'Points')
+    print(Subject[i], Marks[i], Grade[i], Points[i], 'Points')
