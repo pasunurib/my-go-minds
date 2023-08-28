2770622729cb70d5386b9585c35b0efaaefe990a
Pasunurib <Prakashbanu362@gmail.com>
2023-08-28 12:23:10 +0530

    devops

---
+++ Strings/strings_001.py
@@ -0,0 +1,17 @@
+data = str("""true friendship is what gives us reason to stay strong in life. 
+   Having a loving family and all is okay but you also need true friendship to be completely happy. 
+   Some people don't even have families but they have friends who're like their family only. 
+   Thus, we see having true friends means a lot to everyone.""")
+print(data)
+# Print each  first letter should be capital
+print(data.title())
+# Print all in upper case
+print(data.upper())
+print(data.isupper())
+print(data.isalpha())
+print(data.isspace())
+print(data.format())
+print(data.islower())
+print(data.lower())
+print(data.find('friends'))
+print(len(data))
